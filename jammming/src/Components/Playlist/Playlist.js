adacb801b5584b39296256dd0c65cfdefabc11b2
Jason Barrett <barrett.jason.jb@gmail.com>
2022-06-12 10:26:55 +1000

    import class cleanups site now renders

--- jammming/src/Components/Playlist/Playlist.js
+++ jammming/src/Components/Playlist/Playlist.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Playlist.css';
-import Tracklist from '../TrackList/Tracklist';
+import Tracklist from '../TrackList/TrackList';
 
 export class Playlist extends React.Component { 
     render () {
